0e09fe0e9e0c298f1a3937b9b33cde43c0ac6b11
Willian Eliseu da Silva <w.eliseu@gmail.com>
2022-05-22 19:20:29 -0300

    atualização na página de login, adicionada a caixa de diálogo

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,6 @@
 const formLogin = document.getElementById('form-login');
 const formCadastro = document.getElementById('form-cadastro');
+const mensagens = document.getElementById('dialogMessage');
 
 formLogin.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -12,12 +13,17 @@ formLogin.addEventListener('submit', function(e) {
 
 formCadastro.addEventListener('submit', function(e) {
     e.preventDefault();
-    let controle = "cadastro";
-    sendForm(controle);
+    const formattedData = {
+        nome: this.nomeCadastro.value,
+        email: this.emailCadastro.value,
+        celular: this.celularCadastro.value,
+        cpf: this.cpfCadastro.value,
+        senha: this.senhaCadastro.value
+    }
+    sendFormCadastro(formattedData);
 })
 
 async function sendFormLogin(x) {
-    const formData = new FormData(formLogin);
     var bodyContent = JSON.stringify(x);
 
     const response = await fetch("https://test.wl.tv.br/eliseu/nutrisys/apinutrisys/api/login.php", {
@@ -35,6 +41,41 @@ async function sendFormLogin(x) {
     sessionStorage.setItem('cpf', data.cpf);
     sessionStorage.setItem('nome', data.nome);
     sessionStorage.setItem('id', data.id);
+    sessionStorage.setItem('celular', data.celular);
 
     window.location.href = "./areadousuario";
 }
+
+async function sendFormCadastro(x) {
+    var bodyContent = JSON.stringify(x);
+
+    const response = await fetch("https://test.wl.tv.br/eliseu/nutrisys/apinutrisys/api/create.php", {
+        method: "POST",
+        body: bodyContent,
+        header: {
+            'Content-type': 'application/x-www-form-urlencoded'
+        },
+        redirect: 'follow'
+    });
+
+    const data = await response.json();
+
+    if (data.codigo == 1) {
+        //sucesso
+        abrirDialog("Sucesso", data.msg);
+    } else {
+        //erro
+        abrirDialog("Erro", data.msg);
+    }
+}
+
+function abrirDialog(x, y) {
+    document.getElementById('dialogHeader').innerHTML = x;
+    document.getElementById('dialogBody').innerHTML = y;
+    mensagens.setAttribute('open', true);
+    mensagens.classList.remove('msgFechada');
+}
+
+document.getElementById('dialogClose').addEventListener('click', function() {
+    mensagens.classList.add('msgFechada');
+})
